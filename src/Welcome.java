4d303977af5290d8edb5782ed19286e513601ab9
Shrishti Shah <shubhasritadepalli@gmail.com>
2023-12-19 20:11:48 +0530

    Welcome

---
+++ src/Welcome.java
@@ -0,0 +1,5 @@
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        System.out.println("................Welcome to Employee Wage Computation..................");
+    }
+}
