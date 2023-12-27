a2063b5f203b490387bb3a9aec1058878e049e4d
Shrishti Shah <shubhasritadepalli@gmail.com>
2023-12-27 22:41:55 +0530

    Day6

--- src/EmpWageBuilderUC1.java
+++
@@ -1,7 +0,0 @@
-public class EmpWageBuilderUC1{
-    public static void main(String[] args){
-        double check=Math.floor(Math.random()*10)%2;
-        if(check==1) System.out.println("Employee is present");
-        else System.out.println("Employee is Absent");
-    }
-}
